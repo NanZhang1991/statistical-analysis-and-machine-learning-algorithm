6d4f22318a113a40d95429f933b05f30d9936660
nanzhang1991 <nanzhang1991@gmail.com>
2020-10-13 11:32:10 +0800

    update

--- model/deccsionTree.py
+++ model/deccsionTree.py
@@ -68,7 +68,7 @@ def majorityCnt(classList):
         if vote not in classCount.keys(): 
             classCount[vote] = 0
             classCount[vote] += 1
-    sortedClassCount = sorted(classCount.iteritems(),
+    sortedClassCount = sorted(classCount.items(),
                               key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
